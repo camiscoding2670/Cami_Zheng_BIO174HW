b7bb2e06fd089c7da881c4e4f23f891f653074a4
camiscoding2670 <156947148+camiscoding2670@users.noreply.github.com>
2024-04-30 17:17:56 -0700

    Update movie_recommender.py

--- Final Project/movie_recommender.py
+++ Final Project/movie_recommender.py
@@ -1,40 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[52]:
+# In[56]:
 
 
 import streamlit as st
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
-    
+
 # Load the dataset
 def load_data():
     return pd.read_csv('new_dataset.csv')
 
 movies_df = load_data()
+genres = movies_df['genres'].str.split(', ').explode().unique()    
+production_companies = movies_df['production_companies'].str.split(', ').explode().unique()
 
 # Define function to filter and display movies
 def filter_and_display_movies():
     st.title('Welcome to Movie Recommender')
-    st.header('BIO174 Final Project by Cami Zheng')
+    st.subheader('BIO174 Final Project by Cami Zheng')
 
     # Sidebar filters
     st.sidebar.title('Movie Filters')
 
-    # Budget range
-    budget_range = st.sidebar.slider('Select Budget Range', min_value=0, max_value=movies_df['budget'].max(), step=1000000)
-
     # Genre
-    genres = movies_df['genres'].str.split(', ').explode().unique()
     selected_genres = st.sidebar.multiselect('Select Genre', genres)
 
     # Keywords
     keywords = st.text_input("I'm looking for...")
     if keywords:
         st.write("You entered: ", keywords)
-    
     
     # Original Language
     original_language = st.sidebar.selectbox('Select Original Language', movies_df['original_language'].unique())
@@ -43,6 +40,8 @@ def filter_and_display_movies():
     popularity_min = st.sidebar.slider('Select Minimum Popularity', min_value=movies_df['popularity'].min(), max_value=movies_df['popularity'].max(), step=0.1)
     popularity_max = st.sidebar.slider('Select Maximum Popularity', min_value=movies_df['popularity'].min(), max_value=movies_df['popularity'].max(), step=0.1)
     popularity_range = (popularity_min, popularity_max)  # Ensure it's a tuple
+
+    selected_production_companies = st.sidebar.multiselect('Select production companies', production_companies)
 
     # Release Year
     release_date = st.sidebar.number_input('Select release year', min_value=int(movies_df['release_date'].min()), max_value=int(movies_df['release_date'].max()))
@@ -57,12 +56,12 @@ def filter_and_display_movies():
 
     # Filter movies based on selected criteria
     filtered_movies = movies_df[
-        (movies_df['budget'] >= budget_range)&
         (movies_df['genres'].str.split(', ').explode().isin(selected_genres))&
         (movies_df['keywords'].str.contains(keywords, na=False))&
         (movies_df['original_language'] == original_language)&
         (movies_df['popularity'] > popularity_range[0])&
         (movies_df['popularity'] < popularity_range[1])&
+        (movies_df['production_companies'].str.split(', ').explode().isin(selected_production_companies))&
         (movies_df['release_date'] == release_date)&  # Compare formatted release_date
         (movies_df['vote_average'] > vote_average_range[0])&
         (movies_df['vote_average'] < vote_average_range[1])
@@ -70,7 +69,7 @@ def filter_and_display_movies():
     # Check if any movies are found
     if not filtered_movies.empty:
         # Display movie recommendations
-        st.title('Movie Recommendations')
+        st.header('Movie Recommendations')
         for index, movie in filtered_movies.head(5).iterrows():
             st.write(f"Title: {movie['title']}")
             st.write(f"Overview: {movie['overview']}")
@@ -100,7 +99,21 @@ def filter_and_display_movies():
                         st.write(f"Title: {movie['title']}")
                         st.write(f"Overview: {movie['overview']}")
                         st.write(f"Link:{movie['homepage']}")
+# Define function to reset all filters
+def reset_filters():
+    st.sidebar.selectbox('Select Original Language', movies_df['original_language'].unique(), index=0, key="original_language")
+    st.sidebar.slider('Select Minimum Popularity', min_value=movies_df['popularity'].min(), max_value=movies_df['popularity'].max(), step=0.1, value=movies_df['popularity'].min(), key="popularity_min")
+    st.sidebar.slider('Select Maximum Popularity', min_value=movies_df['popularity'].min(), max_value=movies_df['popularity'].max(), step=0.1, value=movies_df['popularity'].max(), key="popularity_max")
+    st.sidebar.multiselect('Select production companies', production_companies)
+    st.sidebar.multiselect('Select Genre', genres)
+    st.sidebar.number_input('Select release year', min_value=int(movies_df['release_date'].min()), max_value=int(movies_df['release_date'].max()), value=int(movies_df['release_date'].min()), key="release_date")
+    st.sidebar.slider('Select Minimum Vote Average', min_value=movies_df['vote_average'].min(), max_value=movies_df['vote_average'].max(), step=0.1, value=movies_df['vote_average'].min(), key="vote_average_min")
+    st.sidebar.slider('Select Maximum Vote Average', min_value=movies_df['vote_average'].min(), max_value=movies_df['vote_average'].max(), step=0.1, value=movies_df['vote_average'].max(), key="vote_average_max")
 
+# Reset button
+if st.sidebar.button("Reset", type="primary"):
+    reset_filters()
+    
 # Call the function to filter and display movies
 filter_and_display_movies()
 
