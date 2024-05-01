27c9fb2205d8654f51847eef09d307c2dc9c6f2f
camiscoding2670 <156947148+camiscoding2670@users.noreply.github.com>
2024-04-30 17:09:37 -0700

    updates

--- Final Project/movie_recommender.py
+++ Final Project/movie_recommender.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[49]:
+# In[52]:
 
 
 import streamlit as st
@@ -17,6 +17,9 @@ movies_df = load_data()
 
 # Define function to filter and display movies
 def filter_and_display_movies():
+    st.title('Welcome to Movie Recommender')
+    st.header('BIO174 Final Project by Cami Zheng')
+
     # Sidebar filters
     st.sidebar.title('Movie Filters')
 
@@ -31,6 +34,7 @@ def filter_and_display_movies():
     keywords = st.text_input("I'm looking for...")
     if keywords:
         st.write("You entered: ", keywords)
+    
     
     # Original Language
     original_language = st.sidebar.selectbox('Select Original Language', movies_df['original_language'].unique())
@@ -49,7 +53,7 @@ def filter_and_display_movies():
     vote_average_max = st.sidebar.slider('Select Maximum Vote Average', min_value=movies_df['vote_average'].min(), max_value=movies_df['vote_average'].max(), step=0.1)
     vote_average_range = (vote_average_min, vote_average_max)  # Ensure it's a tuple
 
-    st.button("Reset", type="primary")
+    st.sidebar.button("Reset", type="primary")
 
     # Filter movies based on selected criteria
     filtered_movies = movies_df[
@@ -71,31 +75,31 @@ def filter_and_display_movies():
             st.write(f"Title: {movie['title']}")
             st.write(f"Overview: {movie['overview']}")
             st.write(f"Link:{movie['homepage']}")
-    else:
-        # No movies found, recommend based on similar genre or rating
-        st.warning('No movies found with the selected criteria. Recommending similar movies...')
-        # Calculate TF-IDF matrix for genres
-        tfidf_vectorizer = TfidfVectorizer()
-        tfidf_matrix = tfidf_vectorizer.fit_transform(movies_df['genres'].fillna(''))
-        # If at least one genre is selected
-        if selected_genres:
-             # Calculate cosine similarity between movies based on genres
-            genre_similarity = cosine_similarity(tfidf_matrix, tfidf_matrix)
-            # Get movies similar to the selected genre
-            similar_movies_idx = genre_similarity[movies_df.index[movies_df['genres'].str.contains(selected_genres[0])].tolist()].argsort(axis=1)[:, ::-1][:, 1]
-            # Display similar movies
-            for idx in similar_movies_idx[:5]:
-                movie = movies_df.iloc[idx]
-                st.write(f"Title: {movie['title']}")
-                st.write(f"Overview: {movie['overview']}")
-                st.write(f"Link:{movie['homepage']}")
         else:
-            st.warning('No genre selected. Recommending top-rated movies...')
-            top_rated_movies = movies_df.sort_values(by='vote_average', ascending=False).head(5)
-            for index, movie in top_rated_movies.iterrows():
-                st.write(f"Title: {movie['title']}")
-                st.write(f"Overview: {movie['overview']}")
-                st.write(f"Link:{movie['homepage']}")
+        # No movies found, recommend based on similar genre or rating
+            st.warning('No movies found with the selected criteria. Recommending similar movies...')
+        # Calculate TF-IDF matrix for genres
+            tfidf_vectorizer = TfidfVectorizer()
+            tfidf_matrix = tfidf_vectorizer.fit_transform(movies_df['genres'].fillna(''))
+        # If at least one genre is selected
+            if selected_genres:
+             # Calculate cosine similarity between movies based on genres
+                genre_similarity = cosine_similarity(tfidf_matrix, tfidf_matrix)
+                # Get movies similar to the selected genre
+                similar_movies_idx = genre_similarity[movies_df.index[movies_df['genres'].str.contains(selected_genres[0])].tolist()].argsort(axis=1)[:, ::-1][:, 1]
+            # Display similar movies
+                for idx in similar_movies_idx[:5]:
+                    movie = movies_df.iloc[idx]
+                    st.write(f"Title: {movie['title']}")
+                    st.write(f"Overview: {movie['overview']}")
+                    st.write(f"Link:{movie['homepage']}")
+                else:
+                    st.warning('No genre selected. Recommending top-rated movies...')
+                    top_rated_movies = movies_df.sort_values(by='vote_average', ascending=False).head(5)
+                    for index, movie in top_rated_movies.iterrows():
+                        st.write(f"Title: {movie['title']}")
+                        st.write(f"Overview: {movie['overview']}")
+                        st.write(f"Link:{movie['homepage']}")
 
 # Call the function to filter and display movies
 filter_and_display_movies()
